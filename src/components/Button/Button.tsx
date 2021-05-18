c32ca3811c416ddc0397fb6b3715f39ed6392826
Nina Rimsky <ninarimsky@gmail.com>
2021-05-18 17:14:06 +0100

    not using string concatenation to create class names

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,39 @@
 import React from "react";
 
-export default function Button(props: { onClick: () => void; color: string; text: string }) {
-  return (
-    <button
-      type="button"
-      className={`inline-flex justify-center px-4 py-2 text-sm font-medium text-${props.color}-900 bg-${props.color}-100 border border-transparent rounded-md hover:bg-${props.color}-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-${props.color}-500`}
-      onClick={props.onClick}
-    >
-      { props.text }
-    </button>
-  );
+export default function Button(props: {
+  onClick: () => void;
+  color: string;
+  text: string;
+}) {
+  if (props.color === "yellow") {
+    return (
+      <button
+        type="button"
+        className={`inline-flex justify-center px-4 py-2 text-sm font-medium text-yellow-900 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-yellow-500`}
+        onClick={props.onClick}
+      >
+        {props.text}
+      </button>
+    );
+  } else if (props.color === "red") {
+    return (
+      <button
+        type="button"
+        className={`inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500`}
+        onClick={props.onClick}
+      >
+        {props.text}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        type="button"
+        className={`inline-flex justify-center px-4 py-2 text-sm font-medium text-green-900 bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500`}
+        onClick={props.onClick}
+      >
+        {props.text}
+      </button>
+    );
+  }
 }
