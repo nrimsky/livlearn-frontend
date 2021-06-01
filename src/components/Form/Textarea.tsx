f164a42913923c05ce3abbc891f6af16209332e8
Nina Rimsky <ninarimsky@gmail.com>
2021-06-01 23:27:43 +0100

    textarea styling

--- src/components/Form/Textarea.tsx
+++ src/components/Form/Textarea.tsx
@@ -14,7 +14,7 @@ export default function Textarea(props: {
         </span>
         <textarea
           rows={2}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-y overflow-hidden"
+          className="resize-y overflow-hidden appearance-none border border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
           placeholder={props.placeholder}
           value={props.value}
           onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
